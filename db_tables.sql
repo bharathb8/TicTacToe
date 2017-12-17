54c6f0560892834a4faddbf8f360c3ac25a55399
root <root@ip-10-164-228-185.ec2.internal>
2017-12-17 19:22:10 +0000

    Update mysql script to create game moves table

--- db_tables.sql
+++ db_tables.sql
@@ -1,6 +1,7 @@
--- create games table
-create table `games`(
-`id` INT NOT NULL,
+ -- create necessary game tables
+DROP TABLE IF EXISTS `ttt_games`;
+CREATE TABLE `ttt_games`(
+`id` INT NOT NULL AUTO_INCREMENT,
 `channel_id` VARCHAR(15) NOT NULL,
 `player1_id` VARCHAR(15) NOT NULL,
 `player2_id` VARCHAR(15) NOT NULL,
@@ -11,3 +12,18 @@ create table `games`(
 `date_modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `game_moves`;
+CREATE TABLE `game_moves`(
+`id` INT NOT NULL AUTO_INCREMENT,
+`game_id` INT NOT NULL,
+`move_number` INT NOT NULL,
+`player_id` VARCHAR(15) NOT NULL,
+`game_board` VARCHAR(128) NOT NULL,
+`date_created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY(`id`),
+KEY `game_index` (`game_id`, `move_number`) USING HASH
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
