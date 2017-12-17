64dd0f96ae3045cebe3d1ee6f4d49504e78412d5
root <root@ip-10-164-228-185.ec2.internal>
2017-12-17 10:06:19 +0000

    Formatting in command processor

--- command_processor.py
+++ command_processor.py
@@ -8,20 +8,39 @@ import os
 import sys
 
 from ttt_game import TTTGame
-from config_helper import get_database_helper
 
 class CommandProcessor(object):
 
-	def processCommand(command_parts=[], request_data={}):
-		try:
+	def parseCommand(self, command_string):
+	    command_parts = []
+	    try:
+		command_parts = command_string.split(" ")
+	    except:
+		logger.error("Exception while parsing command string.")
+	    finally:
+		return command_parts
 
-			if not command_parts:
-				return
 
-			if lower(command_parts[0]) == 'status':
-				channel_id = request_data['channel_id']
-				
+	def processCommand(self, command_string="", request_data={}):
+             response_msg = "Empty msg."
+	     try:
+		command_parts = self.parseCommand(command_string) 
+		
+		if not command_parts:
+		    return
+		print "command parts %s " % command_parts
+		if command_parts[0].lower() == 'status':
+		    channel_id = request_data['channel_id']
+		    game_details = TTTGame.getGameDetailsByChannelId(channel_id)
+		    if game_details:
+			status = game_details['status']
+			if status == 1:
+			   response_msg = "Game in progress"
+		    else:
+			response_msg = "No games in the channel's history."
+	    
+	     except:
+		pass
+	     finally:
+		return response_msg
 
-
-		except:
-			pass
